8483d92cd5f180b8dd34c1e7ed3a94ec5ca0a114
Sacha RENAULT <sacha.renault@orange.com>
2024-07-10 11:51:28 +0200

    feat (data): add audio padding container

--- src/data.py
+++ src/data.py
@@ -8,7 +8,7 @@ class DisplayType(Enum):
 
     # Only available for wave display
     STACK = auto()
-    """Only available for wave display"""
+    """Only available for wave display. \nDisplay all the waves in one graph."""
 
     # Display the stft at specified index
     INDEX = auto()
@@ -16,4 +16,22 @@ class DisplayType(Enum):
 
 class AudioPadding:
     NONE = None
-    RPAD = None
+    """No padding at all"""
+
+    RPAD_RCUT = None
+    """
+        Add zeros values on the right if the audio is too small. 
+        Cut the end of the audio if too long.
+    """
+
+    LPAD_LCUT = None
+    """
+        Add zeros on the left if the audio is too small. 
+        Cut the start of the audio if too long 
+    """
+
+    CENTER_RCUT = None
+    """
+        Add zeros in the left and right to center the audio if too small.
+        Cut the end of the audio if too long. 
+    """
