fc710e68516bc759cfc1a9fcfe2e10a27c3f5398
Sacha RENAULT <sacha.renault@orange.com>
2024-07-10 11:50:59 +0200

    fix (config) fix assertion buggin on none type

--- src/config.py
+++ src/config.py
@@ -1,25 +1,32 @@
 class Config:
     def __init__(self,
                  num_channel : int,
+                 sample_rate : int = 44100,
                  n_fft : int = None,
                  hop_length : int = None,
                  audio_length : int = None,
                  ) -> None:
         # Assert some
-        assert audio_length > 0
-        assert hop_length > 0
         assert num_channel > 0
-        assert n_fft > 0
+        assert sample_rate > 0
+        if hop_length is not None:
+            assert hop_length > 0
+        if audio_length is not None:
+            assert audio_length > 0
+        if n_fft is not None:
+            assert n_fft > 0
         
         # Set attribute
         self.n_fft = n_fft 
         self.hop_length = hop_length
         self.audio_length = audio_length
         self.num_channel = num_channel
+        self.sample_rate = sample_rate
 
     def get_istft_kwargs(self) -> dict:
-        kwargs = {
-            "n_fft" : self.n_fft,
-            "hop_length" : self.hop_length
-        }
+        kwargs = {}
+        if self.n_fft is not None:
+            kwargs["n_fft"] = self.n_fft
+        if self.hop_length is not None:
+            kwargs["hop_length"] = self.hop_length
         return kwargs
