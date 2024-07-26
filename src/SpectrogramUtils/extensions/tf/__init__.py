152566660b5b771b30d986e38da35dbac5d7760d
Sacha RENAULT <sacha35340@gmail.com>
2024-07-26 11:48:17 +0200

    Change folder structure Split torch extension and tf extension in two separated folder

---
+++ src/SpectrogramUtils/extensions/tf/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+try:
+    from ...spectrogram.spectrogram_factory_tf import SpectrogramTfFactory
+except ImportError:
+    warnings.warn("tensorflow wasn't found. Use default factory or install tensorflow to use tensorflow factory")
+
+
---
+++ src/SpectrogramUtils/extensions/torch/__init__.py
@@ -0,0 +1,6 @@
+import warnings
+
+try:
+    from ...spectrogram.spectrogram_factory_torch import SpectrogramTorchFactory
+except ImportError:
+    warnings.warn("torch wasn't found. Use default factory or install torch to use torch factory")
